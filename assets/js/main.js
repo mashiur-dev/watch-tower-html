acd733720f2aa5a5650adcbfaabc8b95d7c5cfb3
Mashiur Rahman <mr.shoyeb21@gmail.com>
2021-08-17 23:32:39 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,106 @@
+(function ($) {
+    "use strict";
+
+    
+    /*--
+        Commons Variables
+    -----------------------------------*/
+    var $window = $(window),
+        $body = $('body');
+
+
+    /*--
+        Off Canvas Function
+    -----------------------------------*/
+    (function () {
+        var $offCanvasToggle = $('.offcanvas-toggle'),
+            $offCanvas = $('.offcanvas'),
+            $offCanvasOverlay = $('.offcanvas-overlay'),
+            $mobileMenuToggle = $('.mobile-menu-toggle');
+        $offCanvasToggle.on('click', function (e) {
+            e.preventDefault();
+            var $this = $(this),
+                $target = $this.attr('href');
+            $body.addClass('offcanvas-open');
+            $($target).addClass('offcanvas-open');
+            $offCanvasOverlay.fadeIn();
+            if ($this.parent().hasClass('mobile-menu-toggle')) {
+                $this.addClass('close');
+            }
+        });
+        $('.offcanvas-close, .offcanvas-overlay').on('click', function (e) {
+            e.preventDefault();
+            $body.removeClass('offcanvas-open');
+            $offCanvas.removeClass('offcanvas-open');
+            $offCanvasOverlay.fadeOut();
+            $mobileMenuToggle.find('a').removeClass('close');
+        });
+    })();
+
+    /*--
+        Off Canvas Menu
+    -----------------------------------*/
+    function mobileOffCanvasMenu() {
+        var $offCanvasNav = $('.offcanvas-menu, .overlay-menu'),
+            $offCanvasNavSubMenu = $offCanvasNav.find('.sub-menu');
+
+        /*Add Toggle Button With Off Canvas Sub Menu*/
+        $offCanvasNavSubMenu.parent().prepend('<span class="menu-expand"></span>');
+
+        /*Category Sub Menu Toggle*/
+        $offCanvasNav.on('click', 'li a, .menu-expand', function (e) {
+            var $this = $(this);
+            if ($this.attr('href') === '#' || $this.hasClass('menu-expand')) {
+                e.preventDefault();
+                if ($this.siblings('ul:visible').length) {
+                    $this.parent('li').removeClass('active');
+                    $this.siblings('ul').slideUp();
+                    $this.parent('li').find('li').removeClass('active');
+                    $this.parent('li').find('ul:visible').slideUp();
+                } else {
+                    $this.parent('li').addClass('active');
+                    $this.closest('li').siblings('li').removeClass('active').find('li').removeClass('active');
+                    $this.closest('li').siblings('li').find('ul:visible').slideUp();
+                    $this.siblings('ul').slideDown();
+                }
+            }
+        });
+    }
+    mobileOffCanvasMenu();
+
+    /*--
+        Scroll Up
+    -----------------------------------*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-long-arrow-up"></i>',
+    });
+
+    /*---- CounterUp ----*/
+    $('.count').counterUp({
+        delay: 10,
+        time: 1000
+    });
+        
+ /*----------------------------
+    	price Plus Minus Button
+    ------------------------------ */
+    var pricePlusMinus = $('.price-plus-minus');
+    pricePlusMinus.prepend('<div class="dec qtybutton">-</div>');
+    pricePlusMinus.append('<div class="inc qtybutton">+</div>');
+    $(".qtybutton").on("click", function() {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() === "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
+})(jQuery);
